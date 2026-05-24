3a7c40a88e625c462600d9a66fe8ca7571228167
pixeel1337 <kamilgraczyk1@gmail.com>
2026-05-24 21:29:56 +0200

    Added parent validation

--- models/Horse.js
+++ models/Horse.js
@@ -51,4 +51,45 @@ const horseSchema = mongoose.Schema({
 
 horseSchema.index({name: 1, birthCountry: 1, birthYear: 1}, {unique: true});
 
+
+horseSchema.pre("save", async function (next) {
+    const horseBirthYear = this.birthYear;
+
+    if(this.father) {
+        const fatherHorse = await mongoose.model("Horse").findById(this.father);
+
+        if(!fatherHorse) {
+            return next(new Error("Podany ojciec nie istnieje w bazie!"));
+        }
+
+        if(fatherHorse.gender !== "ogier") {
+            return next(new Error("Ojciec może być wyłącznie ogierem!"));
+        }
+
+        const fatherAge = horseBirthYear - fatherHorse.birthYear;
+        if(fatherAge < 3 || fatherAge > 21) {
+            return next(new Error("Ojciec musi mieć od 3 do 21 lat!"));
+        }
+    }
+
+    if(this.mother) {
+        const motherHorse = await mongoose.model("Horse").findById(this.mother);
+
+        if(!motherHorse) {
+            return next(new Error("Podana matka nie istnieje w bazie!"));
+        }
+
+        if(motherHorse.gender !== "ogier") {
+            return next(new Error("Matka może być wyłącznie klaczą!"));
+        }
+
+        const motherAge = horseBirthYear - motherHorse.birthYear;
+        if(motherAge < 3 || motherAge > 21) {
+            return next(new Error("Matka musi mieć od 3 do 21 lat!"));
+        }
+    }
+        
+    next();
+});
+
 export const Horse = mongoose.model("Horse", horseSchema);
