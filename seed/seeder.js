d8da9ddea5f883473f7f022a35c1060cd967e307
pixeel1337 <kamilgraczyk1@gmail.com>
2026-06-06 14:53:10 +0200

    Added seeder.js

--- seed/seeder.js
+++ seed/seeder.js
@@ -1,51 +1,37 @@
 import mongoose from "mongoose";
 import { faker } from "@faker-js/faker";
-import { fakerPL } from "@faker-js/faker";
-import { Country } from "./models/Country.js";
-import { Breeder } from "./models/Breeder.js";
-import { Horse } from "./models/Horse.js";
+import { Country } from "../models/Country.js";
+import { Breeder } from "../models/Breeder.js";
+import { Horse } from "../models/Horse.js";
 
 const URL = "mongodb://127.0.0.1:27017/rodowody";
 
-
 const ointmentEnum = ["siwa", "gniada", "kasztanowata", "kara"];
+const genderEnum = ["ogier", "klacz", "wałach"];
 let globalCountries = [];
 let globalBreeders = [];
 let horseCounter = 1;
 
-async function generateAncestor(childBirthYear, forcedGender, currentGeneration) {
+async function generateFatherLine(childBirthYear, currentGeneration) {
     if (currentGeneration > 6) return null;
 
-    const fatherAgeBirth = faker.number.int({ min: 3, max: 21});
+    const fatherAgeBirth = faker.number.int({ min: 3, max: 21 });
     const fatherBirthYear = childBirthYear - fatherAgeBirth;
 
-    const motherAgeBirth = faker.number.int({ min: 3, max: 21});
-    const motherBirthYear = childBirthYear - motherAgeBirth;
+    const fatherID = await generateFatherLine(fatherBirthYear, currentGeneration + 1);
+    const motherID = await generateMotherLine(fatherBirthYear, currentGeneration + 1);
 
-    const fatherID = await generateAncestor(fatherBirthYear, "ogier", currentGeneration + 1);
-    const motherID = await generateAncestor(motherBirthYear, "klacz", currentGeneration + 1);
-
-
-    // Ustawiamy domyślny prefiks
-    let prefix = "Koń";
-    
-    // Jeśli to pokolenie 2, to jest to bezpośredni rodzic źrebaka
-    if (currentGeneration === 2) prefix = forcedGender === "ogier" ? "Ojciec" : "Matka";
-    
-    // Jeśli to pokolenie 3, to są to dziadkowie
-    if (currentGeneration === 3) prefix = forcedGender === "ogier" ? "Dziadek" : "Babcia";
-    
-    // Jeśli pokolenie 4 lub wyższe, to są to pradziadkowie
-    if (currentGeneration >= 4) prefix = forcedGender === "ogier" ? `Pradziadek-P${currentGeneration}` : `Prababcia-P${currentGeneration}`;
-
+    let prefix = "Ojciec";
+    if (currentGeneration === 3) prefix = "Dziadek (ojciec ojca)";
+    if (currentGeneration >= 4) prefix = `Pradziadek-P${currentGeneration}`;
 
     const ancestor = await Horse.create({
-        name: faker.person.firstName(),
-        birthYear: childBirthYear,
-        gender: forcedGender,
+        name: `${prefix} ${faker.person.firstName()} #${horseCounter++}`,
+        birthYear: fatherBirthYear,
+        gender: "ogier",
         ointment: faker.helpers.arrayElement(ointmentEnum),
-        birthCountry: globalCountries[Math.floor(Math.random * globalCountries.length)]._id,
-        breeder: globalBreeders[Math.floor(Math.random * globalBreeders.length)]._id,
+        birthCountry: globalCountries[Math.floor(Math.random() * globalCountries.length)]._id,
+        breeder: globalBreeders[Math.floor(Math.random() * globalBreeders.length)]._id,
         father: fatherID,
         mother: motherID
     });
@@ -53,6 +39,32 @@ async function generateAncestor(childBirthYear, forcedGender, currentGeneration)
     return ancestor._id;
 }
 
+async function generateMotherLine(childBirthYear, currentGeneration) {
+    if (currentGeneration > 6) return null;
+
+    const motherAgeBirth = faker.number.int({ min: 3, max: 21 });
+    const motherBirthYear = childBirthYear - motherAgeBirth;
+
+    const fatherID = await generateFatherLine(motherBirthYear, currentGeneration + 1);
+    const motherID = await generateMotherLine(motherBirthYear, currentGeneration + 1);
+
+    let prefix = "Matka";
+    if (currentGeneration === 3) prefix = "Babcia (matka matki)";
+    if (currentGeneration >= 4) prefix = `Prababcia-P${currentGeneration}`;
+
+    const ancestor = await Horse.create({
+        name: `${prefix} ${faker.person.firstName()} #${horseCounter++}`,
+        birthYear: motherBirthYear,
+        gender: "klacz",
+        ointment: faker.helpers.arrayElement(ointmentEnum),
+        birthCountry: globalCountries[Math.floor(Math.random() * globalCountries.length)]._id,
+        breeder: globalBreeders[Math.floor(Math.random() * globalBreeders.length)]._id,
+        father: fatherID,
+        mother: motherID
+    });
+
+    return ancestor._id;
+}
 
 async function seedDatabase() {
     try {
@@ -66,11 +78,17 @@ async function seedDatabase() {
 
         console.log("3. Generowanie bazy krajów...");
         const countriesToInsert = [];
-        for(let i = 0; i < 15; i++) {
-            countriesToInsert.push({
-                name: faker.location.country(),
-                code: faker.location.countryCode()
-            });
+        const usedCodes = new Set();
+
+        while (countriesToInsert.length < 15) {
+            const code = faker.location.countryCode();
+            if (!usedCodes.has(code)) {
+                usedCodes.add(code);
+                countriesToInsert.push({
+                    name: faker.location.country(),
+                    code: code
+                });
+            }
         }
         globalCountries = await Country.insertMany(countriesToInsert);
 
@@ -86,12 +104,30 @@ async function seedDatabase() {
 
         console.log("Baza krajów oraz hodowców została pomyślnie wygenerowana!");
 
-        ////////////////////////////////////////////////////////////////
-        //////          Dodanie Obsługi                           //////
-        //////                Koni                                //////
-        ///////////////////////////////////////////////////////////////
+        console.log("Generowanie 10 źrebaków i ich drzew genealogicznych...");
+        for(let i = 0; i < 10; i++) {
+            const mainHorseBirthYear = faker.number.int({ min: 2022, max: 2026 });
+            const gender = faker.helpers.arrayElement(genderEnum);
+
+            const fatherID = await generateFatherLine(mainHorseBirthYear, 2);
+            const motherID = await generateMotherLine(mainHorseBirthYear, 2);
+
+            await Horse.create({
+                name: `Źrebak ${faker.person.firstName()} #${horseCounter++}`,
+                birthYear: mainHorseBirthYear,
+                gender: gender,
+                ointment: faker.helpers.arrayElement(ointmentEnum),
+                birthCountry: globalCountries[Math.floor(Math.random() * globalCountries.length)]._id,
+                breeder: globalBreeders[Math.floor(Math.random() * globalBreeders.length)]._id,
+                father: fatherID,
+                mother: motherID
+            });
+        }
+        
+        console.log("SUKCES! Baza danych została wypełniona!");
+
     } catch(err) {
-        console.error("Błą podczas generowania danych: " + err.message);
+        console.error("Błąd podczas generowania danych: " + err.message);
     } finally {
         await mongoose.disconnect();
         console.log("Odłączono od bazy danych!");
